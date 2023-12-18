f284d7a50be3be201283d8059c12c428933246ed
Ivan Shepelev <shepelevivan11032006@gmail.com>
2023-12-18 18:02:07 +0200

    initial commit

---
+++ 07-IndexSignature/index.ts
@@ -0,0 +1,40 @@
+interface IFirstInterface {
+    [key: string | number]: number
+}
+
+interface ISecondInterface {
+    [key: string]: (...rest: any[]) => {}
+}
+
+
+interface ArrayLikeObject {
+    [index: number]: number;
+
+    length: number;
+}
+
+
+interface Interface {
+    [key: string]: string | number | boolean
+
+    name: string
+}
+
+interface IParent {
+    [key: string]: string | number
+}
+
+interface IChild extends IParent {
+    name: string
+    age: number;
+}
+
+
+function checkKeys(obj: { [key: string]: number }, keysToCheck: string[]): boolean {
+    for (const key of keysToCheck) {
+        if (typeof obj[key] !== 'number') {
+            return false;
+        }
+    }
+    return true;
+}
